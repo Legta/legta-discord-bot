1e8e7b4ea6be332739a92fe1d0a444092e945ac9
Legta <11740065+Legta@users.noreply.github.com>
2024-03-05 20:56:53 -0500

    GuildMembers and GuildPresences intents added to check for members and their presences for spotify command

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const fs = require('node:fs');
 const path = require('node:path');
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]});
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildPresences]}); //some of these intents need to be enabled in the discord developer portal
 
 client.commands = new Collection;
 
