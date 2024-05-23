5fb1dec78290dadbdeef709c17123899b0cb9f3f
Legta <edgarsimonzambrano@gmail.com>
2024-05-22 20:52:16 -0500

    added model selection to generate command

--- commands/utility/generate.js
+++ commands/utility/generate.js
@@ -1,12 +1,15 @@
 const { SlashCommandBuilder } = require('discord.js')
 const hordeApi = 'https://stablehorde.net/api/'
-const {hordeKey} = require('../../config.json')
+const { hordeKey } = require('../../config.json')
 
 const headers = {
     'apikey': hordeKey,
     'Content-Type': 'application/json'
 };
 
+let currentModels = []
+
+fetch(hordeApi + '/v2/status/models').then(response => response.json()).then(result => result.map(model => currentModels.push(model.name)))
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -15,19 +18,42 @@ module.exports = {
         .addStringOption((option) =>
             option.setName('prompt').setRequired(true).setDescription('Prompt to generate!')
         )
+        .addStringOption((option) => option
+            .setName('model')
+            .setDescription('Choose which model to use for generation')
+            .setRequired(true)
+            .setAutocomplete(true)
+        )
         .addStringOption((option) =>
             option.setName('number').setRequired(false).setDescription('Number of images to generate (4 max)')
-        ),
+        )
+    ,
+
+    async autocomplete(interaction) {
+        const focusedValue = interaction.options.getFocused();
+        if (focusedValue === '') return await interaction.respond(
+            currentModels.slice().splice(0, 24).map(choice => ({name: choice, value: choice}))
+        )
+        if (currentModels.length > 0) {
+            const choices = currentModels
+            // console.log('focused', focusedValue)
+            const filtered = choices.filter(choice => choice.toLowerCase().startsWith(focusedValue));
+            await interaction.respond(
+                filtered.map(choice => ({ name: choice, value: choice })),
+            );
+        }
+    },
 
     async execute(interaction) {
         await interaction.deferReply()
         const prompt = interaction.options.getString('prompt').toLowerCase()
-        const numberOfImages = interaction.options.getString('number')? interaction.options.getString('number') : 1
+        const numberOfImages = interaction.options.getString('number') ? interaction.options.getString('number') : 1
+        const selectedModel = interaction.options.getString('model')
         const parsedBatchSize = parseInt(numberOfImages)
-        console.log(numberOfImages)
+        console.log('Selected model:', selectedModel)
         const randomSeed = generateSeed()
         const body = JSON.stringify({
-            "prompt": prompt,
+            "prompt": selectedModel === 'Pony Diffusion XL'? 'score_9, score_8_up, score_7_up, score_6_up, score_5_up, score_4_up ' + prompt : prompt,
             "params": {
                 "cfg_scale": 7,
                 "seed": randomSeed,
@@ -37,16 +63,14 @@ module.exports = {
                 "width": 512,
                 "steps": 30,
                 "tiling": false,
-                "clip_skip": 1,
+                "clip_skip": selectedModel === 'Pony Diffusion XL' ? 2 : 1,
                 "n": parsedBatchSize > 0 ? parsedBatchSize < 5 ? parsedBatchSize : 4 : 1
             },
             "nsfw": true,
             "karras": true,
             "censor_nsfw": false,
             "trusted_workers": true,
-            "models": ["Pastel Mix"],
-            // "models": ["iCoMix"],
-            // "models": ["CamelliaMix 2.5D"],
+            "models": [selectedModel],
             "r2": true,
             "replacement_filter": true,
             "shared": false,
@@ -81,12 +105,13 @@ module.exports = {
                 if (check.done) {
                     const image = await fetch(hordeApi + `/v2/generate/status/${genId}`).then(response => response.json()).then(result => result)
                     const attachments = image.generations.map(generation => ({ attachment: generation.img, name: 'generation.png' }))
-                    interaction.followUp({content: `Generation finished for prompt "${prompt}" <@${interaction.user.id}>! \nCheck it out:`, files: attachments })
+                    interaction.followUp({ content: `Generation finished for prompt "${prompt}" <@${interaction.user.id}>! \nCheck it out:`, files: attachments })
                     console.log('Checking done')
+                    console.log(check)
+                    console.log(image)
                     return clearInterval(interval)
                 }
                 console.log(`generating... \nWait time: ${check.wait_time}\nQueue position: ${check.queue_position}`)
-                console.log(check)
             }
         }, 2000)
     }
