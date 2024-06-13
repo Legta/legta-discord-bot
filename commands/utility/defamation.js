d9fb757ca2e6ae75673b4225a24530a50c39e56b
Legta <edgarsimonzambrano@gmail.com>
2024-06-13 18:59:26 -0500

    added defamation command that uses hermahs api

---
+++ commands/utility/defamation.js
@@ -0,0 +1,16 @@
+const {SlashCommandBuilder, AttachmentBuilder} = require('discord.js')
+const stream = require('node:stream')
+
+module.exports = {
+    data: new SlashCommandBuilder().setName('defamation').setDescription('Shows a random hermahs defamation'),
+
+    async execute (interaction) {
+        await interaction.deferReply()
+        await interaction.followUp('Defamating rn...')
+        const image = await fetch('http://api.hermahs.com/defamation', {'Content-Type': 'image'}).then(response => response)
+        if (!image.ok) return interaction.followUp("Couldn't retrieve image...")
+        const contentType = image.headers.get('content-type')
+        const attachment = new AttachmentBuilder().setFile(image.body).setName(`hermahs_defamation.${contentType.split('/').pop()}`)
+        await interaction.followUp({files: [attachment]})
+    }
+}
