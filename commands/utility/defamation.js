722016b6698bf3128c29d759ccd6985079de9516
Legta <11740065+Legta@users.noreply.github.com>
2024-10-19 17:20:49 -0500

    Changed the way that defamations are sent to the channel (It now deletes the original deferred message)

--- commands/utility/defamation.js
+++ commands/utility/defamation.js
@@ -4,12 +4,14 @@ module.exports = {
     data: new SlashCommandBuilder().setName('defamation').setDescription('Shows a random hermahs defamation'),
 
     async execute (interaction) {
-        await interaction.deferReply()
-        await interaction.followUp('Defamating rn...')
+        await interaction.deferReply();
+        await interaction.followUp('Defamating rn...');
+        const deferredMessage = await interaction.fetchReply("@original"); //gets the deferred reply to delete it later
         const image = await fetch('http://api.hermahs.com/defamation', {'Content-Type': 'image'}).then(response => response)
         if (!image.ok) return interaction.followUp("Couldn't retrieve image...")
         const contentType = image.headers.get('content-type')
         const attachment = new AttachmentBuilder().setFile(image.body).setName(`hermahs_defamation.${contentType.split('/').pop()}`)
-        await interaction.editReply({content: '', files: [attachment]})
+        await interaction.channel.send({content: '', files: [attachment]}) //sends the image to the channel without replying
+        await deferredMessage.delete()
     }
 }
