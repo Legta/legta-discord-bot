0e8d4d754b84df76525adc210d1cc7c4319f622b
Legta <11740065+Legta@users.noreply.github.com>
2024-10-21 22:32:37 -0500

    Early implementation of the quote context command

--- commands/utility/quote.js
+++ commands/utility/quote.js
@@ -1,11 +1,21 @@
 const { ContextMenuCommandBuilder, ApplicationCommandType, AttachmentBuilder } = require('discord.js');
-const { Jimp } = require('jimp');
+const { Jimp, loadFont, HorizontalAlign, VerticalAlign } = require('jimp');
+const { SANS_32_WHITE } = require("jimp/fonts")
 const path = require('path');
 const fs = require('node:fs');
 
-const imageSize = "800x400"
-const avatarSize = "400x400"
-const backgroundSize = "687x400"
+const canvasSize = {
+    width: 800,
+    height: 400
+}
+const avatarSize = {
+    width: 400,
+    height: 400,
+}
+const backgroundSize = {
+    width: 687,
+    height: 400,
+}
 const textTransforms = {
     "x" : 477,
     "y" : 168,
@@ -20,17 +30,47 @@ module.exports = {
 
     async execute (interaction) {
 
-        const deferred = await interaction.deferReply({ephemeral: false, fetchReply: true}); //Stores the deferred message
-        const messageText = interaction.targetMessage.content || "";
-        const templateImgPath = path.join(__dirname, "assets", "quote_template.png");
-        const baseImage = await Jimp.read(templateImgPath);
+        try {
+            if (interaction.targetMessage.author.bot) return interaction.reply({content: "Can't quote a bot...", ephemeral: true});
+            const messageText = interaction.targetMessage.content || "";
+            const deferred = await interaction.deferReply({ephemeral: false, fetchReply: true}); //Stores the deferred message
+    
+            const templateImgPath = path.join(__dirname, "assets", "quote_template.png");
+            const backgroundPng = await Jimp.read(templateImgPath);
+            const avatarImageLink = interaction.targetMessage.author.avatarURL({extension: "png", size: 512}); //Gets the link with png extension, otherwise it gets webp and Jimp can't work with that
+            const avatarImage = await Jimp.read(await getAvatar(avatarImageLink));
+            const canvas = new Jimp({ width: canvasSize.width, height: canvasSize.height, color: 0xffffffff }); //Creates the canvas to then composite the images on top of it;
+            const font = await loadFont(SANS_32_WHITE);
+            const italicFont = await loadFont(path.join(__dirname, "assets", "CrimsonTextItalic.fnt"));
+    
+            avatarImage.resize({w: avatarSize.width, h: avatarSize.height})
+            canvas.composite(avatarImage)
+            canvas.composite(backgroundPng)
+            let resultCoords = {}; // Creating variable for results of end of text after printing the quote, to get the position of it and use it to offset the name of the user a little bit.
+            canvas.print({font, x: textTransforms.x - 65, y: textTransforms.y - textTransforms.y, maxWidth: 350, maxHeight: 370, text: {text: `"${messageText}"`, alignmentX: HorizontalAlign.CENTER, alignmentY: VerticalAlign.MIDDLE}, cb: (results) => resultCoords = results})
+            /* Line 52: 412 is textTransforms.x - 65, 65 is the offset I gave it by trial and error on line 50, this makes the username centered accounting for the offsets*/
+            canvas.print({font: italicFont, x: textTransforms.x + 65, y: resultCoords.y + 10, text: {text: `-${interaction.targetMessage.author.displayName}`}})
 
-        const imageResponse = new AttachmentBuilder().setFile(templateImgPath).setName("hermahs_quote.png")
-
-        setTimeout(async () => {
+            const img = await canvas.getBuffer("image/png"); //creates a buffer that is readable by the attachment builder
+            const imageResponse = new AttachmentBuilder().setFile(img).setName("hermahs_quote.png");
+    
+            
             await deferred.delete();
-            await interaction.channel.send({content: messageText, files: [imageResponse]});
-        }, 2500);
+            await interaction.channel.send({content: "", files: [imageResponse]});
+            
+        } catch (error) {
+            console.log(error)
+        }
+
     }
 
+}
+
+async function getAvatar (link) {
+    try {
+        const response = await fetch(link);
+        return await response.arrayBuffer();
+    } catch (error) {
+        return console.log(error)
+    }
 }
