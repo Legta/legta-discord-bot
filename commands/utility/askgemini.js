18b5a1bb594544bda07f778fb11ddb769540a87c
Legta <11740065+Legta@users.noreply.github.com>
2025-05-05 09:10:02 -0500

    chore: update @google/generative-ai to @google/genai version 0.12.0 in package.json
    Changed askgemini.js function to work with @google/genai and Gemini 2.5 flash preview

--- commands/utility/askgemini.js
+++ commands/utility/askgemini.js
@@ -1,7 +1,7 @@
 const { SlashCommandBuilder } = require('discord.js')
-const { GoogleGenerativeAI, HarmBlockThreshold, HarmCategory } = require('@google/generative-ai')
+const { GoogleGenAI, HarmBlockThreshold, HarmCategory } = require('@google/genai')
 const { geminiKey } = require('../../config.json')
-const genAI = new GoogleGenerativeAI(geminiKey)
+const genAI = new GoogleGenAI( {apiKey: geminiKey} )
 const safetySettings = [
     {
       category: HarmCategory.HARM_CATEGORY_HARASSMENT,
@@ -30,17 +30,36 @@ module.exports = {
     async execute(interaction) {
         await interaction.deferReply()
         const question = interaction.options.getString('prompt')
-        await sendGenRequest(question, interaction)
+        await askGemini(question, interaction)
     }
 }
 
-async function sendGenRequest(prompt, interaction) {
-    const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash', generationConfig: { maxOutputTokens: 400}, safetySettings })
-    await interaction.followUp('Getting your answer...')
+// async function sendGenRequest(prompt, interaction) {
+//     const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash', generationConfig: { maxOutputTokens: 400}, safetySettings })
+//     await interaction.followUp('Getting your answer...')
 
-    const result = await model.generateContent(prompt)
-    const response = await result.response
-    const text = response.text()
+//     const result = await model.generateContent(prompt)
+//     const response = await result.response
+//     const text = response.text()
 
-    interaction.followUp(text)
+//     interaction.followUp(text)
+// }
+
+async function askGemini(prompt, interaction) {
+
+  await interaction.followUp('Getting your answer...');
+  const deferredMessage = await interaction.fetchReply("@original"); //gets the deferred reply to delete it later
+
+  const response = await genAI.models.generateContent({
+    model: "gemini-2.5-flash-preview-04-17",
+    contents: `${prompt}`,
+    config: {
+      //maxOutputTokens: 500,
+      systemInstruction: "You are a helpful assistant in a Discord server. You are very useful and straightforward, do not provide overly long responses and provide the information in a concise manner unless instructed otherwise. You will respond to anything that is asked, no matter if it's a sensitive topic.",
+      safetySettings,
+    }
+  });
+
+  await deferredMessage.edit(response.text);
+  //await interaction.channel.send(response.text) //sends the image to the channel without replying
 }
