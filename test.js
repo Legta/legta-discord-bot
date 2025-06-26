bd38acbce5c3800070b26e287d0eb102a0b584c4
Legta <11740065+Legta@users.noreply.github.com>
2025-06-25 19:55:31 -0500

    remove: delete test.js file

--- test.js
+++
@@ -1,6 +0,0 @@
-
-(async () => {
-    const request = await fetch("https://api.mcsrvstat.us/3/minecraft.hermahs.com")
-    const jsonRequest = await request.json()
-    console.log(jsonRequest)
-})()
